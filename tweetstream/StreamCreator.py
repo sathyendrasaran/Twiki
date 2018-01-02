2020becefe13e723ad99e661f8a0066f4990cc4a
Sathyendrasaran Murali <sathyendrasaran@gmail.com>
2018-01-02 13:01:31 -0800

    Update StreamCreator.py

--- tweetstream/StreamCreator.py
+++ tweetstream/StreamCreator.py
@@ -2,8 +2,8 @@ import boto3
 
 client = boto3.client('kinesis',
                       region_name='us-west-2',
-                      aws_access_key_id='AKIAJ2CQZHOYGX5AB4DA',
-                      aws_secret_access_key='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')
+                      aws_access_key_id='XXXXXXXX',
+                      aws_secret_access_key='XXXXXXXX')
 response = client.create_stream(
     StreamName='twitter',
     ShardCount=1
