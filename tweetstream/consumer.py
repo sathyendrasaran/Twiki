c84b867ce34e4cbb172d837dc0bf2fdf558b002f
Sathyendrasaran Murali <sathyendrasaran@gmail.com>
2018-01-02 13:02:00 -0800

    Update consumer.py

--- tweetstream/consumer.py
+++ tweetstream/consumer.py
@@ -3,8 +3,8 @@ import time
 import json
 ## aws creds are stored in ~/.boto
 kinesis = boto3.client("kinesis",
-aws_access_key_id='AKIAJ2CQZHOYGX5AB4DA',
-aws_secret_access_key='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')
+aws_access_key_id='XXXXXXXX',
+aws_secret_access_key='XXXXXXXX')
 shard_id = "shardId-000000000000" #only one shard!
 pre_shard_it = kinesis.get_shard_iterator(StreamName="twitter", ShardId=shard_id, ShardIteratorType="LATEST")
 shard_it = pre_shard_it["ShardIterator"]
