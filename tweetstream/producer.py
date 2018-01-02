3fb5de50f16213ee6e9b883edbe78367c2a7e0fa
Sathyendrasaran Murali <sathyendrasaran@gmail.com>
2018-01-02 13:03:20 -0800

    Update producer.py

--- tweetstream/producer.py
+++ tweetstream/producer.py
@@ -4,17 +4,17 @@ import json
 
 ## twitter credentials
 
-consumer_key = "mIkoyZg5hgTul8VLb0AyIAfdV"
-consumer_secret = "NDETD7PDY9SfgQwgSVwVD7zb1WJHijM8FUOACFzfNdyM63qSfi"
-access_token_key = "155263055-COpdWg8pVMuwYMmUscIZrdLwpni5l7EN2Mzf7TNM"
-access_token_secret = "7qC6fwBL2Xhy2Nj5ekZlOcKLHMVNo6dJxTXbciTAH02hk"
+consumer_key = "XXXXXXXX"
+consumer_secret = "XXXXXXXX"
+access_token_key = "XXXXXXXX-XXXXXXXX"
+access_token_secret = "XXXXXXXX"
 
 api = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_secret)
 
 kinesis = boto3.client('kinesis',
 region_name='us-west-2',
-aws_access_key_id='AKIAJ2CQZHOYGX5AB4DA',
-aws_secret_access_key='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')
+aws_access_key_id='XXXXXXXX',
+aws_secret_access_key='XXXXXXXXXXXXXXXX')
 
 r = api.request('statuses/filter', {'locations':'-90,-90,90,90'})
 
